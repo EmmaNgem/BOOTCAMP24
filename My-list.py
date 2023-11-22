c6eb4ff79281508c9f3046d8db3310992e87bafc
EmmaNgem <150918541+EmmaNgem@users.noreply.github.com>
2023-11-21 20:50:05 -0800

    Add files via upload

---
+++ My-list.py
@@ -0,0 +1,63 @@
+#exercise1
+print('hello world\n' * 4)
+
+
+
+#exercise2
+print(int(9**3) * 8)
+
+
+
+#exercise3
+input(5<3)
+input(3 == 3)
+input(3 == "3")
+input("3" > 3)
+input("hello" == "hello")
+
+
+#exercise4
+computer_brand = "Dell"
+print("I have a" + computer_brand + "computer")
+
+
+#ecercise5
+name= "Emma"
+age= 20
+shoe_size= 38
+info= ("my nameis" + name + "i am" + age + "and my shoe size" + shoe_size)
+
+
+#excercise6
+a=20
+b=30
+if a > b:
+   print('hello world ')
+
+
+#exercise7
+number = int(input('Enter a number'))
+if number % 2:
+    print("even")
+else:
+    print("odd")
+
+
+#exercise8
+user=input("what is your name ")
+name="Emmanuella"
+if user == name:
+   print("its me")
+else:
+    print("its not me")
+number=input=( "")
+
+
+    #exercise 9
+number=40
+height=int(input("enter a" + number))
+cm= height * 2.5
+if cm > 145:
+    print("they are tall enough to ride ")
+else:
+    print("need to grow some more ti ride")
