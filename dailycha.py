1eb6df4d2c1e5397bc0ba341f310c6e3af3f1a57
EmmaNgem <150918541+EmmaNgem@users.noreply.github.com>
2023-11-21 20:38:35 -0800

    Add files via upload

---
+++ dailycha.py
@@ -0,0 +1,21 @@
+name=int("input a string ('1, 10')")
+if name < 10:
+    print('string is not long enough ')
+if name > 10:
+    print("string is tii lond")
+if name==10:
+   print("perfect string")
+ # 2
+   print(1 and 10)
+#3
+word="typewritting"
+text= str('input' + word)
+for t in word:
+    print(t)
+    #4
+import random
+text='helloworld'
+random.shuffle(text)
+print(word)
+
+
