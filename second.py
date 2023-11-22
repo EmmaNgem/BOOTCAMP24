719cc3688cdd391b6f49c7e4bd00ef3195e88c0f
EmmaNgem <150918541+EmmaNgem@users.noreply.github.com>
2023-11-21 21:18:01 -0800

    Add files via upload

--- second.py
+++ second.py
@@ -1,2 +1,12 @@
-print(" hello world " * 4 ," I love python " * 4)
-number=input(int("enter a month between  and 12"))
+
+print(" hello world\n " * 4 ," I love python \n" * 4)
+month=(1,2,3,4,5,6,7,8,9,10,11,12)
+number=input(int("enter a" + month + "between 1 and 12"))
+if number==3, 5:
+    print('Sring')
+if number==6, 12:
+    print{summer}
+if number==9, 11:
+    print(Autumn)
+if number==12, 2:
+    print(winter)
