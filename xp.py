dd03790abff2cb6598bf5b968b01aee2da255749
EmmaNgem <150918541+EmmaNgem@users.noreply.github.com>
2023-11-22 06:40:14 -0800

    Add files via upload

---
+++ xp.py
@@ -0,0 +1,46 @@
+favs={ 1, 2, 3, 4, 5}
+favs .add(7)
+print(favs)
+favs.add(10)
+   print(favs)
+favs.remove(10)
+frfav={ 5, 4, 3, 2, 1}
+ourfavs=(favs + frfavs)
+   print(ourfavs)
+#ex2
+tupple=(5, 6, 20, 15, 2, 3, 5)
+tupple.add(50)
+print(tupple)
+#3
+
+Basket=["Banana", "apples", "Orsnges", "Blueberries"]
+Basket.remove(Banana)
+   print(Basket)
+Basket.remove(blueberries)
+   print(Basket)
+basket.apend(kiwi)
+   print(Basket)
+basket.add(apples)
+   print(Basket)
+Basket.sum(apples)
+   print(Basket)
+basket.remove()
+#ex4 integers are whole while floats are decimals
+#we can also create them as fractions
+list=[1.5, 2, 2.5, 3, 3.5, 4, 4.5]
+#ex5
+numbers=range(1, 21)
+for number in numbers:
+    print(numbers)
+if number % 2==0:
+    print(number)
+    #ex6
+    name="Emmanuella"
+    while input("what is your name:") != "Emmanuella"
+    ...continue
+
+
+
+    print(shirts[0]["name"])
+
+
