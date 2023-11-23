4339c837e3c77d98a56fb9fe679ca310e2ad45aa
EmmaNgem <150918541+EmmaNgem@users.noreply.github.com>
2023-11-23 01:29:35 -0800

    Add files via upload

---
+++ Exexp.py
@@ -0,0 +1,100 @@
+Keys=['ten', 'twenty','thirty']
+values=[10, 20, 30]
+for item in zip( key, values):
+    print(item)
+    #ex2
+age=int("input a numberr" .range(2, 15))
+if age<3:
+    print("tiket is free")
+if age<12:
+    print("tiket is 10$")
+if age>12:
+    print("tiket is 15$")
+    
+familly={"rick": 43, 'beth': 13, "morth": 5, 'summer': 8}
+values={range(8, 43)}
+for range in values, print(values)
+if values =8:
+    print("summer will  pay 10$")
+if values =5:
+    print("Morth will pay 10$")
+if values= 13:
+    print("beth will pay 10$")
+if values=43:
+    print("rick will pay 15$")
+
+    #4
+rick=15
+beth=10
+morth=10
+summer=10
+input=.sum(rick, beth, morth, summer :)
+   print("the families total cost of watching movies is", input)
+
+#5
+input={}
+
+
+#ex3
+name: Zara 
+creation_date: 1975 
+creator_name: Amancio Ortega Gaona 
+type_of_clothes: men, women, children, home 
+international_competitors: Gap, H&M, Benetton 
+number_stores: 7000 
+major_color: 
+    France: blue, 
+    Spain: red, 
+    US: pink, green
+    name: Zara 
+
+Brand={
+    'creation_date': '1975 '
+    'counry_creation': 'spain'
+    'creator_name': 'Amancio Ortega Gaona' 
+    'type_of_clothes': '[men, women, children, home]' 
+    'international_competitors': '[Gap, H&M, Benetton,]' 
+    'number_stores': 7000
+    'major_color': 
+    {'France': 'blue', 
+     'Spain': 'red', 
+    'US' : ['pink', 'green']
+    }
+    }
+    print(major_color "Zara\s customers are wemen")
+for list in Brand:
+input=.remove("creation_date")
+   print(input)
+for international_competitors in Brand:
+input=.add( Desigual)
+    print(input)
+for international_competitors in Brand:
+    print(the last international competitor is (2))
+for major_color in Brand:
+input=.print(US)
+    print(input)
+more_on_Zara={
+    'creation_date': '1975' 
+    'number_stores': '10 000'
+}
+input=.apend(more_on_Zara :)
+   print(input)
+for dict in more_on_Zara:
+    print(number stores)
+
+
+
+    #ex4
+disney_users_A=
+{"Mickey": 0, "Minnie": 1, "Donald": 2, "Ariel": 3, "Pluto": 4}
+for names in disney_users_A:
+    print(names)
+
+    
+
+
+
+
+
+
+
