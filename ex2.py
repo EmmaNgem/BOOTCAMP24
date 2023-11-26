c0230504c292defa526eebee193886e62f3e2215
EmmaNgem <150918541+EmmaNgem@users.noreply.github.com>
2023-11-26 18:14:32 +0100

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,30 @@
+#ex3
+3 <= 3 < 9
+3==3==3
+bool(0)
+bool(5 == "5")
+bool(5 =="5")
+bool(4 == 4) == bool("4" == "4")
+bool(bool(None))
+x = (1 == True)
+y = (1 == False)
+a = True + 4
+b = False + 10
+
+print("x is", x)
+print("y is", y)
+print("a:", a)
+print("b:", b)
+#4
+
+my_text = ["Lorem", "ipsum", "dolor", "amet", "consectetur", "adipiscing", "elit",]
+count=my_text.count()
+print(count)
+
+user=input("the longes sentence without the letter A:")
+sentence=input()
+if sentence is not long:
+    print(sentence)
+
+ 
+
